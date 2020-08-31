14e3994e143454d1546d7647db0bd9eb7154c402
Oswaldo Cristovam <oswaldo.jose.c@hotmail.com>
2020-08-31 14:59:43 -0300

    Acrescentando comentários aos posts

--- src/main/java/com/ojcfcl/workshop_springboot_mongdb/domain/Post.java
+++ src/main/java/com/ojcfcl/workshop_springboot_mongdb/domain/Post.java
@@ -1,12 +1,15 @@
 package com.ojcfcl.workshop_springboot_mongdb.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import com.ojcfcl.workshop_springboot_mongdb.dto.AuthorDTO;
+import com.ojcfcl.workshop_springboot_mongdb.dto.CommentDTO;
 
 @Document(collection = "post")
 public class Post implements Serializable {
@@ -18,6 +21,8 @@ public class Post implements Serializable {
 	private String title;
 	private String doby;
 	private AuthorDTO author;
+	
+	private List<CommentDTO> comments = new ArrayList<>();
 	
 	public Post() {
 	}
@@ -78,6 +83,14 @@ public class Post implements Serializable {
 		return result;
 	}
 
+	public List<CommentDTO> getComments() {
+		return comments;
+	}
+
+	public void setComments(List<CommentDTO> comments) {
+		this.comments = comments;
+	}
+	
 	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)

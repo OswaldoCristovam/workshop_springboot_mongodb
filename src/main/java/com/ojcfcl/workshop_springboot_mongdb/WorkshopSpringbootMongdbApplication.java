ea924cbbed63e53fd775bcbd16a9954b7d73759c
Oswaldo Cristovam <oswaldo.jose.c@hotmail.com>
2020-08-31 10:33:10 -0300

    Primeiro commit - projeto criado

---
+++ src/main/java/com/ojcfcl/workshop_springboot_mongdb/WorkshopSpringbootMongdbApplication.java
@@ -0,0 +1,13 @@
+package com.ojcfcl.workshop_springboot_mongdb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkshopSpringbootMongdbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkshopSpringbootMongdbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ojcfcl/workshop_springboot_mongdb/WorkshopSpringbootMongdbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ojcfcl.workshop_springboot_mongdb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopSpringbootMongdbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

c0bd2643142e5447c291822e9af2083c95b5f6f3
Oswaldo Cristovam <oswaldo.jose.c@hotmail.com>
2020-08-31 13:46:45 -0300

    Inserção de usuário com POST

--- src/main/java/com/ojcfcl/workshop_springboot_mongdb/service/UserService.java
+++ src/main/java/com/ojcfcl/workshop_springboot_mongdb/service/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.ojcfcl.workshop_springboot_mongdb.domain.User;
+import com.ojcfcl.workshop_springboot_mongdb.dto.UserDTO;
 import com.ojcfcl.workshop_springboot_mongdb.repository.UserRepository;
 import com.ojcfcl.workshop_springboot_mongdb.service.exception.ObjectNotFoundException;
 
@@ -23,5 +24,13 @@ public class UserService {
 	public User findById(String id) {
 		Optional<User> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+	
+	public User insert(User obj) {
+		return repo.insert(obj);
+	}
+	
+	public User fromDTO(UserDTO objDto) {
+		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
 	}
 }
